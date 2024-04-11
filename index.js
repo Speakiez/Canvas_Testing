45f0003e2410dbc5320fabf42b4d00c4d539d928
Speakiez <aerholgocalin57@gmail.com>
2024-04-11 21:22:15 +0800

    Test using an OOP approach to drawing

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+window.onload = () => {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+}
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+class Pixel {
+    constructor(x, y, id) {
+        this.position = {
+            x,
+            y
+        }
+        this.id = id;
+        this.width = 1;
+        this.height = 1;
+    }
+
+    draw() {
+        ctx.fillStyle = "#E3FEF7";
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+const onePx = new Pixel(50, 50, 0);
+onePx.draw();
